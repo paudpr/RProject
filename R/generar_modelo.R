7e24a9c364388d8e3a9da7ff8c1eb57d4738673d
AlbaDu <you@example.com>
2020-07-07 17:30:06 +0200

    Multiple Linear Regression

--- R/generar_modelo.R
+++ R/generar_modelo.R
@@ -14,11 +14,12 @@ generar_modelo <- function(datos, config){
   num_train <- round(nrow(datos) * 0.7) #numero de datos para entrenar
   posicion_train <- sample(1:nrow(datos), size = num_train) #posiciones de los datos para entrenar en el DF
   
-  train <- datos[posicion_train] #conjunto de datos para train
-  test <- datos[-posicion_train] #conjunto de datos para test
+  train <- datos[posicion_train,] #conjunto de datos para train
+  test <- datos[-posicion_train,] #conjunto de datos para test
   
-  #modelo 1 --> LinearRegression
-  
+  #modelo 1 --> Multiple LinearRegression
+  linearMod <- lm(ener_pers ~ ., data = train)#data es el grupo d datos xra entrenar?
+  print(linearMod)
   
   #modelo 2 --> SGDRegression 
   
