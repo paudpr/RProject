c7651b52fa73080653300db44540b9c73ab6dc59
AlbaDu <you@example.com>
2020-07-09 13:45:05 +0200

    todos los modelos menos RandomForest

--- R/generar_modelo.R
+++ R/generar_modelo.R
@@ -4,6 +4,7 @@
 #' @param config 
 #'
 #' @import sgd
+#' @import e1071
 #' @import logging
 #'
 #' @examples
@@ -18,20 +19,29 @@ generar_modelo <- function(datos, config){
   test <- datos[-posicion_train,] #conjunto de datos para test
 
   #modelo 1 --> Multiple LinearRegression
-  linear_mod <- lm(ener_pers ~ ., data = train) #data es el grupo d datos xra entrenar?
+  linear_mod <- lm(train$ener_pers ~ ., data = train) #data es el grupo d datos xra entrenar
   print(linear_mod)
   linear_mod_summary <- summary(linear_mod)
-  linear_mod_coeffs <- linear_mod_summary$coefficients
   
-  test$height_predic <- predict(linear_mod, test)
-  linear_mod_acc <- abs(sum(test$height - test$height_predic)/nrow(test))
+  test$ener_pers_predic <- predict(linear_mod, test)
+  linear_mod_acc <- abs(sum(test$ener_pers - test$ener_pers_predic)/nrow(test))
   print(linear_mod_acc)
     
   #modelo 2 --> SGDRegression 
+  sgd_mod <- sgd(train$ener_pers ~ ., data = train, model = "lm")
+  print(sgd_mod)
   
+  test$ener_pers_predic <- predict(sgd_mod, test)
+  sgd_mod_acc <- abs(sum(test$ener_pers - test$ener_pers_predic)/nrow(test))
+  print(sgd_mod_acc)
   
-  #modelo 3 --> SVM
+  #modelo 3 --> SVR Linear
+  svr_mod <- svm(train$ener_pers ~ ., data = train)
+  svr_mod
   
+  test$ener_pers_predic <- predict(svr_mod, test)
+  svr_mod_acc <- abs(sum(test$ener_pers - test$ener_pers_predic)/nrow(test))
+  print(svr_mod_acc)
   
   #modelo 4 --> RandomForest
 }
