7ff0e562ebd5c857edf159e22e0e40fffcf9eb0a
Paula del Pino Ramírez <pauladelpinoramirez@MacBook-Pro-de-Paula-2.local>
2020-07-09 16:44:36 +0200

    añadir limpieza nas

--- R/preproceso.R
+++ R/preproceso.R
@@ -27,6 +27,57 @@ preproceso <- function(lista_csv, config){
   }
   
   
+  
+  
+  #' Title
+  #'
+  #' @param datos 
+  #' @param config 
+  #'
+  #' @return
+  #' @import logging
+  #'
+  #' @examples
+  #' @export
+  
+  
+  
+  preproceso_limpieza <- function(df, config){
+    
+    #check columnas a utilizar
+    columnas_utilizadas <- c(config$columnas$index$pais, config$columnas$index$ano,
+                             config$columnas$predictoras$cre_pobl, config$columnas$predictoras$cre_urb,
+                             config$columnas$predictoras$den_pobl, config$columnas$predictoras$ener_pers, 
+                             config$columnas$predictoras$esp_vida.csv)
+    
+    comprobacion_columnas <- columnas_utilizadas%in%colnames(df)
+    comprobacion <- all(comprobacion_columnas)
+    
+    if(!comprobación){
+      columnas_no <- columnas_utilizadas[!comprobacion_columnas]
+      logerror(paste0("Las columnas:", paste(columnas_no, collapse = ","),
+                      " no se encuentran en el dataframe."), logger = "log")
+      stop()
+      
+    }
+    
+    
+    df <- df[,columnas_utilizadas]
+    
+    
+  limpieza_nas <- function(df, config){
+      #para cada valor en NA de una columna de las predicción en el config, eliminar columna
+      df <- subset(df, select = -c(is.na(config$columnas$index$pais), is.na(config$columnas$index$ano)))
+      
+      #para cada valor en NA de una fila q, eliminar la fila
+      if (df$country != config$columnas$index$pais, config$columnas$index$ano){
+        datos_1 <- df[complete.cases(df),]
+        complete_rows <- na.omit(df)
+      }
+  }
+    
+    
+    
     
     
     #merge(x, y, by = c("k1","k2"))
